db6bc30bd357d3a4cd5da3d6f84c0396a4f5487a
Wontae Yang <wontaeyang@gmail.com>
2016-11-26 17:55:56 -0500

    add background

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var Two = require('./two.js')
-
 var body = document.body;
+
+addBackdrop(30);
+
 var two = new Two({
   fullscreen: true,
   autostart: true
@@ -11,7 +13,10 @@ function Node() {
   this.posY = 30;
   this.width = 30;
   this.height = 30;
+  this.selected = false;
   this.body = two.makeRectangle(this.posX, this.posY, this.width, this.height);
+
+  // Initial setup of node
   this.init = function() {
     this.body.fill = 'rgb(0, 200, 255)';
     this.body.opacity = 0.75;
@@ -20,9 +25,24 @@ function Node() {
 
     // Add click event
     this.body._renderer.elem.addEventListener('click', function(){
-      this.body.fill = 'red';
+      if (this.selected) {
+        this.deselect();
+        this.selected = false;
+      } else {
+        this.select();
+        this.selected = true;
+      }
+
       two.update();
     }.bind(this), false)
+  }
+  this.select = function() {
+    this.body.stroke = 'orangered';
+    this.body.linewidth = 3;
+
+  }
+  this.deselect = function () {
+    this.body.noStroke();
   }
   this.init();
 }
@@ -32,3 +52,29 @@ function draw() {
 }
 
 draw();
+
+
+
+function addBackdrop(d) {
+  var dimensions = d || 50;
+  var two = new Two({
+    type: Two.Types.canvas,
+    width: dimensions,
+    height: dimensions
+  });
+
+  var r = dimensions / 10;
+  var center = dimensions / 2;
+
+  var a = two.makeLine(center - r, center, center + r, center);
+  var b = two.makeLine(center, center - r, center, center + r);
+
+  a.stroke = b.stroke = '#aaa';
+  a.linewidth = b.linewidth = 0.25;
+
+  two.update();
+
+  body.style.backgroundImage = 'url(' + two.renderer.domElement.toDataURL() + ')'
+  body.style.backgroundRepeat = 'repeat'
+  body.style.backgroundSize = dimensions + 'px ' + dimensions + 'px'
+}
